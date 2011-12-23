9ab9eb1873f5b558e6594b285215db6c7251f5a7
Junji Nishino <nishinojunji@gmail.com>
2011-12-24 02:15:36 +0900

    dellower added

--- card.c
+++ card.c
@@ -7,21 +7,28 @@
 #include<stdio.h>
 #include<stdint.h>
 
+typedef int64_t myint64;
+
 typedef struct {
-  int64_t c; /* bit board sizeof(long) == 8 in 64bit architecture */
+  myint64 c; /* bit board sizeof(long) == 8 in 64bit architecture */
   int na; /* number of all cards */
 } Cards;
 
 
+/* ----- prototypes ----- */
 void cards_display(Cards c);
 int cards_countnum(Cards c);
 Cards cards_addcard(Cards c, int i);
 Cards cards_init();
+Cards cards_dellower(Cards c, Cards d);
 
+
+/* ----- main ----- */
 main(){
-  Cards c;
+  Cards c, d;
 
   c = cards_init();
+  d = cards_init();
 
   c = cards_addcard(c, 0);
   c = cards_addcard(c, 3);
@@ -30,14 +37,20 @@ main(){
   c = cards_addcard(c, 30);
   c = cards_addcard(c, 31);
   c = cards_addcard(c, 32);
-  /* c = cards_addcard(c, 33); */
-  /* c = cards_addcard(c, 60); */
-  /* c = cards_addcard(c, 40); */
-  /* c = cards_addcard(c, 50); */
+  c = cards_addcard(c, 33);
+  c = cards_addcard(c, 60);
+  c = cards_addcard(c, 40);
+  c = cards_addcard(c, 50);
   c = cards_addcard(c, 7);
-  cards_display(c);
-  printf("\n");
-  printf("countnum %d\n", cards_countnum(c));
+  cards_display(c);  printf("\n");
+
+  d = cards_addcard(d, 31);
+  cards_display(d);  printf("\n");
+  
+  /*  printf("countnum %d\n", cards_countnum(c)); */
+
+  cards_display(cards_dellower(c, d)) ;  printf("\n");
+
 }
 
 
@@ -54,6 +67,14 @@ int cards_countnum(Cards c){
   return num;
 }
 
+/* delete cards in c lower than d */
+Cards cards_dellower(Cards c, Cards d){
+  myint64 mask;
+  mask = ((~d.c)+1) ^ d.c; /* 00001000 -> 11110000 */
+  c.c = c.c & mask;
+  return c;
+}
+
 Cards cards_init(){
   Cards c;
   c.na= 0;
@@ -68,7 +89,7 @@ Cards cards_addcard(Cards c, int i){
 }
 
 void cards_display(Cards c){
-  int64_t a;
+  myint64 a;
   int i;
   i=0;
   a = c.c;
